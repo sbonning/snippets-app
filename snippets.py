bf7afa6e0d87d1d54a3a3af782827e9ff1118fbe
Sam Bonning <sbonning@gmail.com>
2017-06-02 09:38:23 +0200

    updated parsers

--- snippets.py
+++ snippets.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -20,3 +21,37 @@ def get(name):
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
+
+def view_all():
+	"""
+	Views all the current snippets saved and their associated names
+	"""
+	logging.error("FIXME: Inimplemented - view_all()")
+	return ""
+
+def search(string):
+	"""
+	Allows search of all saved snippets and names 
+
+	Returns all snippets containing the string entered
+
+	If there is no such snippet, return 'No snippets found'
+	"""
+	logging.error("FIXME: Unimplemented - search({!r})".format(string))
+	return ""
+
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    #subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="Name of the snippet")
+    put_parser.add_argument("snippet", help="Snippet text")
+
+    arguments = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
